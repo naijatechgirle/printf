09c979d41584db569b3ebe65e533811735738ef5
naijatechgirle <kellyeluagu@gmail.com>
2023-05-28 16:07:43 -0700

    added prototypes to main file

---
+++ main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Importing standard C lib */
+
+#include <unistd.h>
+#include <stdarg.h>
+
+/* function prototypes */
+int _printf(const char *format, ...);
+int _puts(char *s)
+int _putchar(char z);
+int _my_puts(char *string);
+void _our_int(int num);
+int _put_int(int integer);
+
+#endif /* MAIN */
