e8b572c3294ad3a300621a9746fbcac3bf5f36e2
naijatechgirle <kellyeluagu@gmail.com>
2023-05-29 06:31:26 -0700

    fxn that prints to output

---
+++ _printf.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _printf - fxn to print output
+ * @format: input
+ *
+ * Description - fxn to format and print output to the
+ * standard output stream
+ *
+ * Return: 0 on success
+ */
+
+int _printf(const char *format, ...)
+{
+	unsigned int i = 0, i_val = 0;
+
+	va_list args;
+
+	va_start(args, format);
+
+	for (; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+		}
+		else if (format[i + 1] == 'c')
+		{
+			_putchar(va_arg(args, int));
+			i++;
+		}
+		else if (format[i + 1] == 's')
+		{
+			int i_value = _puts(va_arg(args, char *));
+
+			i++;
+			i_value += (i_val--);
+		}
+		else if (format[i + 1] == '%')
+		{
+			_putchar('%');
+			i++;
+		}
+	}
+	return (i_val);
+}
+
